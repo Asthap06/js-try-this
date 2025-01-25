f12631f6d30186cbcfa5578161bd8c46f745a50b
Astha Pandey <164369166+Asthap06@users.noreply.github.com>
2025-01-25 20:08:59 +0530

    Add files via upload
    create file

---
+++ test.js
@@ -0,0 +1,34 @@
+// let,const,and var :
+const accountId=122344
+let accountemail="astha@123gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountState;
+
+//accountId=2
+accountemail="yashu@30gmail.com"
+accountPassword="23456"
+accountCity="noida"
+
+console.log(accountId);
+console.table([accountId,accountemail,accountPassword,accountCity,accountState]);
+
+// datatypes and ECMA standards:
+
+"use strict";//treat all js code as newer version
+
+//alert("hello!") // we are using node js not browser
+
+let name="Astha"//string
+let age=22//numbers,//number=2 to power53,//bigint is used for large no.
+let isLoggedId= false //boolean
+let state;
+
+//null=standard calue
+// undefines,sybol=unique
+
+//object
+
+console.log(typeof"Astha");
+
+
